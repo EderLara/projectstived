bd35c66f2d0ecc44b5bbd6216058568755fd91d9
EderLara <ederlatru@gmail.com>
2022-05-23 21:26:58 -0500

    Modelos: Usuario, Tipo de documento y tipo de usuario

--- backend/rizomas/src/models/user.model.js
+++ backend/rizomas/src/models/user.model.js
@@ -4,3 +4,24 @@
 */
 
 'use strict';
+
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const UsuarioSchema = Schema({
+    NickName : String,                                              // Correo Electrónico de ingreso de usuario
+    PassUser : String,                                              // Password del usuario
+    EstaUser : String,                                              // Estado del Usuario
+    TipoUser : { type : Schema.ObjectId, ref: 'TipoUsuario' },      // Tipo de Usuario
+    DatosUser : {
+        TipoDocu : { type: Schema.ObjectId, ref: 'TipoDocu' },      // Tipo de documento del usuario
+        IdenUser : String,                                          // Número de identificación
+        NombUser : String,                                          // Nombres del Usuario
+        ApelUser : String,                                          // Apellidos del Usuario
+        EmaiUser : String,                                          // Correo Electrónico personal del usuario 
+        ProgUser : String                                           // Programa al que pertenece el usuario
+    }
+})
+
+module.exports = mongoose.model('Usuario', UsuarioSchema)
